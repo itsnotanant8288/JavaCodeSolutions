07f344f9adf9912fdef74ea30f449788aefe6040
Anant Singh <anantsingh8288@gmail.com>
2024-05-01 23:07:48 +0530

    LongestSubstring solution added

--- src/LongestSubstring.java
+++ src/LongestSubstring.java
@@ -39,4 +39,29 @@ public class LongestSubstring {
     }
 }
 
+// Answer Submitted on LeetCode
+//
+//import java.util.HashSet;
+//
+//class Solution {
+//    public int lengthOfLongestSubstring(String s) {
+//        int n = s.length();
+//        HashSet<Character> set = new HashSet<>();
+//        int maxLength = 0, left = 0, right = 0;
+//
+//        while (right < n) {
+//            if (!set.contains(s.charAt(right))) {
+//                set.add(s.charAt(right));
+//                if (right - left + 1 > maxLength) {
+//                    maxLength = right - left + 1;
+//                }
+//                right++;
+//            } else {
+//                set.remove(s.charAt(left));
+//                left++;
+//            }
+//        }
+//        return maxLength;
+//    }
+//}
 
