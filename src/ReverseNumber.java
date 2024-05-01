60f61d98ae70943fbce7b612c49686ffa6a30d43
Anant Singh <anantsingh8288@gmail.com>
2024-05-01 17:14:36 +0530

    ReverseNumber code added

--- src/ReverseNumber.java
+++ src/ReverseNumber.java
@@ -1,8 +1,59 @@
 import java.util.*;
+import java.lang.Math;
 
 public class ReverseNumber {
+    public static int lengthOfNumber(int num)
+    {   int count=0;
+        while(num!=0)
+        {
+            count++;
+            num /= 10;
+        }
+        return count;
+    }
     public static void main(String[] args)
     {
-        System.out.println("Base");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        boolean is_negative = false;
+        if(num < 0)
+        {
+            num *= -1;
+            is_negative = true;
+        }
+
+        if(num < 10)
+        {
+            if(is_negative)
+            {
+                System.out.println("Reversed number : " + (-1 * num));
+            }
+            else
+            {
+                System.out.println("Reversed number : " + num);
+            }
+        }
+        else
+        {
+            int temp=num;
+            int len = lengthOfNumber(num);
+            int opp=0;
+            int mul= (int) Math.pow(10,len-1);
+            while(temp!=0)
+            {
+                opp += temp%10 * mul;
+                temp /= 10;
+                mul /= 10;
+            }
+            if(is_negative)
+            {
+                System.out.println("Reversed number : " + (-1 * opp));
+            }
+            else
+            {
+                System.out.println("Reversed number : " + opp);
+            }
+        }
     }
 }
